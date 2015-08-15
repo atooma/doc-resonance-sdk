d8438c99cdf68abd918b2082be3f5733d1734b0e
Andrea Petreri <andrea.petreri@gmail.com>
2015-08-15 20:09:05 +0200

    Adding sidebar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -221,6 +221,10 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {
+    'index': [],
+    '**': ['atooma_sidebar.html']
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
