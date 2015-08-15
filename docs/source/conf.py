1c14de59e167a387a7b0fe90d82a2b942aee36c0
Andrea Petreri <andrea.petreri@gmail.com>
2015-08-15 20:07:13 +0200

    Theme change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -170,7 +170,7 @@ html_theme_options = {
     #
     # Options are nothing (default) or the name of a valid theme
     # such as "amelia" or "cosmo".
-    'bootswatch_theme': "simplex",
+    'bootswatch_theme': "sandstone",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
